fc31c7a25a93abc3204d28e4aeb7678f8c2b4cfc
WaspHornet <wasphornet@hotmail.com>
2023-09-22 17:50:32 +0700

    Add action button and style
    - add icon
    - add clear button
    - add copy button

--- src/components/token-convert/index.tsx
+++ src/components/token-convert/index.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react"
 import template from '@/templates/convert-token.json'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import TransformIcon from '@mui/icons-material/Transform';
+import ClearIcon from '@mui/icons-material/Clear';
+
 
 const TokenConvert = () => {
     const { template_string: templateString, mapping_key: mappingKey } = template || {}
@@ -41,7 +45,7 @@ const TokenConvert = () => {
     return (
         <div id="token-convert-wrapper" className="text-black min-w-full">
             <h1>TokenConvert</h1>
-            <div id="content-wrapper" className="grid gap-2 my-5">
+            <div id="content-wrapper" className="grid gap-5 my-5">
                 {error && <div className="alert alert-error">
                     <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                     <span>Is invalid data or incorrect format</span>
@@ -54,18 +58,49 @@ const TokenConvert = () => {
                     onChange={(e) => {
                         setError(false)
                         setJsonText(e.target.value)
-                    }} />
-                <button
-                    className="btn btn-active btn-primary"
-                    onClick={onClickConvert}
-                    disabled={!templateString || !mappingKey?.length}
-                >
-                    Convert To Token Web
-                </button>
-            </div>
-
-            <div id="result-textarea-wrapper">
-                <textarea className="textarea textarea-info text-white min-w-full" rows={5} placeholder="Bio" value={result} readOnly />
+                    }}
+                />
+                <div id="button-wrapper" className="flex gap-5 justify-center">
+                    <button
+                        id="convert-button"
+                        className="flex-auto btn btn-active btn-success"
+                        onClick={onClickConvert}
+                        disabled={!templateString || !mappingKey?.length}
+                    >
+                        <TransformIcon />
+                        Convert
+                    </button>
+                    <button
+                        id="copy-button"
+                        className="flex-auto btn btn-active btn-primary"
+                        onClick={() => {
+                            navigator.clipboard.writeText(result)
+                        }}
+                    >
+                        <ContentCopyIcon />
+                        Copy
+                    </button>
+                    <button
+                        id="clear-button"
+                        className="flex-auto btn btn-active btn-error"
+                        onClick={() => {
+                            setJsonText('')
+                            setResult('')
+                        }}
+                    >
+                        <ClearIcon />
+                        Clear
+                    </button>
+                </div>
+                <div id="result-textarea-wrapper">
+                    <textarea
+                        className="textarea textarea-info text-white min-w-full"
+                        rows={5}
+                        placeholder="Bio"
+                        value={result}
+                        readOnly
+                    />
+                </div>
             </div>
 
         </div>
