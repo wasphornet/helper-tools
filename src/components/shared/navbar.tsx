8b2b627ee99ea786d3b6febff1e545de96c4b43f
WaspHornet <wasphornet@hotmail.com>
2023-11-04 22:37:12 +0700

    fix theme error

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -3,56 +3,39 @@ import { firstUpperCase } from "@/utils/helpers"
 import { setTheme } from '@/reducers/theme-reducer'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from "@/store"
+import { themeSite } from '@/utils/constants-value'
 
 const Navbar = () => {
-    // const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false)
     const dispatch = useDispatch()
     const theme = useSelector((state: RootState) => state.theme.value)
     const [checked, setChecked] = useState(false)
 
     const onClickChangeTheme = () => {
         let newTheme
-        if ( theme === 'dracula' ) {
-            newTheme = 'garden'
+        if (theme === themeSite.dark) {
+            newTheme = themeSite.light
         } else {
-            newTheme = 'dracula'
+            newTheme = themeSite.dark
         }
         dispatch(setTheme(newTheme))
         localStorage.setItem('theme', newTheme)
     }
 
     useEffect(() => {
-        setChecked(theme !== 'dracula')
+        setChecked(theme !== themeSite.dark)
     }, [theme])
 
     useEffect(() => {
         let localTheme = String(localStorage.getItem('theme'))
-        if ( !localTheme ) {
-            console.log("🚀 ~ file: navbar.tsx:27 ~ useEffect ~ localTheme:", localTheme)
+        if (localTheme !== themeSite.dark && localTheme !== themeSite.light) {
             localTheme = 'dracula'
         }
         dispatch(setTheme(localTheme))
     }, [])
 
     return (
-        <div className="navbar bg-base-100 flex justify-between p-5 border-b-2 shadow-xl">
-            <div className="mx-5">
-                <p className="text-2xl">Helpers tools</p>
-            </div>
-            <div className="mx-5">
-                <span className="label-text mr-5 font-bold">{firstUpperCase(theme)}</span>
-                <input type="checkbox" className="toggle" checked={checked} onClick={onClickChangeTheme} />
-            </div>
-            {/* <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                </button>
-            </div>
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                </button>
-            </div>
+        <div className="navbar bg-base-100 flex justify-between py-5 px-16 border-b-2 shadow-xl gap-5">
             <div className="drawer">
                 <input id="my-drawer" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
@@ -63,9 +46,17 @@ const Navbar = () => {
                     <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
                         <li><a>Sidebar Item 1</a></li>
                         <li><a>Sidebar Item 2</a></li>
+
                     </ul>
                 </div>
-            </div> */}
+            </div>
+            <div>
+                <p className="text-2xl">Helpers tools</p>
+            </div>
+            <div>
+                <span className="label-text mr-5 font-bold">{firstUpperCase(theme)}</span>
+                <input type="checkbox" className="toggle" checked={checked} onClick={onClickChangeTheme} readOnly />
+            </div>
         </div>
     )
 }
---
+++ src/utils/constants-value.ts
@@ -0,0 +1,6 @@
+export const themeSite = {
+    dark: 'dracula',
+    light: 'garden',
+}
+
+export const themeList = ["dracula", "night", "garden", "light"]
