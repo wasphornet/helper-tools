9c43139fd8e4124398a4fb78604bab0c9bd25e79
WaspHornet <wasphornet@hotmail.com>
2024-01-25 14:48:52 +0700

    add feature encrypt RSA

--- src/components/@shared/navbar/index.tsx
+++ src/components/@shared/navbar/index.tsx
@@ -10,7 +10,8 @@ import { NavbarStyled, DrawerStyled } from './navbar.styled'
 
 const routerList = [
   { routeName: 'Token Convert', pathname: '/' },
-  { routeName: 'Deeplink Generator', pathname: '/deeplink-generator' }
+  { routeName: 'Deeplink Generator', pathname: '/deeplink-generator' },
+  { routeName: 'Encrypt RSA', pathname: '/encrypt-rsa' }
 ]
 
 const Navbar = () => {
